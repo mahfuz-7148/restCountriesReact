96517be0469b208d8ce0b1ea8c4955979e9bae39
mahfuz-7148 <mrahman7148@gmail.com>
2025-03-28 00:13:48 +0600

    added travel list

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -4,20 +4,18 @@ import Country from "./Country.jsx";
 const Countries = ({countries}) => {
     const countriesData = use(countries);
     const [visitedCountries, setVisitedCountries] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+
 
     const handleVisitedCountries = country => {
         setVisitedCountries((prevCountries) => {
-            // চেক করি এই দেশ আগে ভিজিট করা আছে কিনা
             const isCountryVisited = prevCountries.some(
                 (c) => c.name.common === country.name.common
             );
-
-            // যদি না থাকে, তাহলে লিস্টে যোগ করি
             if (!isCountryVisited) {
                 return [...prevCountries, country];
             }
-
-            // যদি থাকে, তাহলে বাদ দিয়ে নতুন লিস্ট দিই
             return prevCountries.filter(
                 (c) => c.name.common !== country.name.common
             );
@@ -29,14 +27,26 @@ const Countries = ({countries}) => {
             <h1 className="text-indigo-700 text-4xl font-medium my-6 text-center">
                 Travelling Countries: {countriesData.length}
             </h1>
-            <h1 className="text-2xl mb-10">Traveled so far: {visitedCountries.length}</h1>
-           <div className='grid grid-cols-3 gap-5 p-3'>
+
+           <div className='flex justify-end'>
+               <h1 className="text-2xl mb-5">Traveled so far: {visitedCountries.length}</h1>
+               <ul className="my-6 text-fuchsia-500 text-2xl text-end">
+                   {visitedCountries.map((country) => (
+                       <li className='text-center' key={country.name.common}>
+                           {country.name.common}
+                       </li>
+                   ))}
+               </ul>
+           </div>
+           <div className='grid grid-cols-4 gap-5 p-3'>
                    {
                        countriesData.map(country => (
                            <Country key={country.cca3} country={country}  onVisitedCountries={handleVisitedCountries}/>
                        ))
                    }
            </div>
+
+
         </div>
     );
 };
