c46e6c4022191c6f49662bcbfeeb642514dc2596
mahfuz-7148 <mrahman7148@gmail.com>
2025-03-27 23:18:31 +0600

    added visited not visited button

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -4,6 +4,26 @@ import Country from "./Country.jsx";
 const Countries = ({countries}) => {
     const countriesData = use(countries);
     const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleVisitedCountries = country => {
+        setVisitedCountries((prevCountries) => {
+            // চেক করি এই দেশ আগে ভিজিট করা আছে কিনা
+            const isCountryVisited = prevCountries.some(
+                (c) => c.name.common === country.name.common
+            );
+
+            // যদি না থাকে, তাহলে লিস্টে যোগ করি
+            if (!isCountryVisited) {
+                return [...prevCountries, country];
+            }
+
+            // যদি থাকে, তাহলে বাদ দিয়ে নতুন লিস্ট দিই
+            return prevCountries.filter(
+                (c) => c.name.common !== country.name.common
+            );
+        });
+    }
+
     return (
         <div className='max-w-screen-2xl mx-auto px-5'>
             <h1 className="text-indigo-700 text-4xl font-medium my-6 text-center">
@@ -13,7 +33,7 @@ const Countries = ({countries}) => {
            <div className='grid grid-cols-3 gap-5 p-3'>
                    {
                        countriesData.map(country => (
-                           <Country key={country.cca3} country={country} />
+                           <Country key={country.cca3} country={country}  onVisitedCountries={handleVisitedCountries}/>
                        ))
                    }
            </div>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,6 +1,9 @@
+import {useState} from "react";
 
 
-const Country = ({country}) => {
+const Country = ({country, onVisitedCountries}) => {
+    const [visited, setVisited] = useState(false);
+
     return (
         <div className='border border-amber-900 p-3 rounded-lg text-2xl'>
             <div className="w-full h-52">
@@ -9,6 +12,13 @@ const Country = ({country}) => {
             <h3>Name: {country.name.common}</h3>
             <p>Independent: {country.independent ? 'Free' : 'Not Free'}</p>
             <p>Population: {country.population}</p>
+            <button
+                onClick={()=> {setVisited(!visited),
+                    onVisitedCountries(country)}}
+                className="border mt-3 border-slate-400 py-2 px-4 rounded-md cursor-pointer"
+            >
+                {visited ? "Visited" : "Not Visited"}
+            </button>
         </div>
     );
 };
