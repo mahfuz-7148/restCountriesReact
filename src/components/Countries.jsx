93e8677245e465bc551ca60175f7740a757fc705
mahfuz-7148 <mrahman7148@gmail.com>
2025-03-28 15:54:21 +0600

    added travel list

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -23,28 +23,31 @@ const Countries = ({countries}) => {
     }
 
     return (
-        <div className='max-w-screen-2xl mx-auto px-5'>
+        <div className='container mx-auto px-5'>
             <h1 className="text-indigo-700 text-4xl font-medium my-6 text-center">
                 Travelling Countries: {countriesData.length}
             </h1>
 
-           <div className='flex justify-end'>
-               <h1 className="text-2xl mb-5">Traveled so far: {visitedCountries.length}</h1>
-               <ul className="my-6 text-fuchsia-500 text-2xl text-end">
-                   {visitedCountries.map((country) => (
-                       <li className='text-center' key={country.name.common}>
-                           {country.name.common}
-                       </li>
-                   ))}
-               </ul>
-           </div>
-           <div className='grid grid-cols-4 gap-5 p-3'>
-                   {
-                       countriesData.map(country => (
-                           <Country key={country.cca3} country={country}  onVisitedCountries={handleVisitedCountries}/>
-                       ))
-                   }
-           </div>
+            <div className='w-full flex justify-between'>
+                <div className='w-3/4 grid grid-cols-4 gap-5 p-3'>
+                    {
+                        countriesData.map(country => (
+                            <Country key={country.cca3} country={country}  onVisitedCountries={handleVisitedCountries}/>
+                        ))
+                    }
+                </div>
+
+                <div className='w-1/4  my-6 text-emerald-600 text-2xl text-center'>
+                    <h1 className="mb-5 text-indigo-700 text-3xl">Traveled so far: {visitedCountries.length}</h1>
+                    <div>
+                        {visitedCountries.map((country) => (
+                            <p key={country.name.common}>
+                                {country.name.common}
+                            </p>
+                        ))}
+                    </div>
+                </div>
+            </div>
 
 
         </div>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -5,20 +5,24 @@ const Country = ({country, onVisitedCountries}) => {
     const [visited, setVisited] = useState(false);
 
     return (
-        <div className='border border-amber-900 p-3 rounded-lg text-2xl'>
-            <div className="w-full h-52">
-                <img className="w-full h-full" src={country.flags.png} alt="" />
+        <div className='border border-amber-900 p-3 rounded-lg text-xl'>
+            <div className="w-full">
+                <img className="w-full h-44" src={country.flags.png} alt="" />
             </div>
-            <h3>Name: {country.name.common}</h3>
-            <p>Independent: {country.independent ? 'Free' : 'Not Free'}</p>
-            <p>Population: {country.population}</p>
-            <button
-                onClick={()=> {setVisited(!visited),
-                    onVisitedCountries(country)}}
-                className="border mt-3 border-slate-400 py-2 px-4 rounded-md cursor-pointer"
-            >
-                {visited ? "Visited" : "Not Visited"}
-            </button>
+          <div className='flex flex-col justify-between items-start gap-y-5'>
+             <div>
+                 <h3>Name: {country.name.common}</h3>
+                 <p>Independent: {country.independent ? 'Free' : 'Not Free'}</p>
+                 <p>Population: {country.population}</p>
+             </div>
+              <button
+                  onClick={()=> {setVisited(!visited),
+                      onVisitedCountries(country)}}
+                  className="border mt-3 border-slate-400 py-2 px-4 rounded-md cursor-pointer"
+              >
+                  {visited ? "Visited" : "Not Visited"}
+              </button>
+          </div>
         </div>
     );
 };
