d804cafcaae32b2fb9eed40cfe53a3cf13c8e10a
mahfuz-7148 <mrahman7148@gmail.com>
2025-03-28 16:57:17 +0600

    added search functionality

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -34,18 +34,21 @@ const Countries = ({countries}) => {
                 Travelling Countries: {countriesData.length}
             </h1>
 
-            <div className="mb-5 text-center">
 
-                <div>
-                    <i className="ri-search-line text-indigo-900 text-2xl"></i>
+            <div className="mb-5 flex justify-center">
+                <div className="relative w-64">
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-indigo-900">
+            <i className="ri-search-line text-2xl"></i>
+          </span>
                     <input
                         value={searchTerm}
-                        onChange={(evt) => setSearchTerm(evt.target.value)}
-                        className="border border-gray-400 rounded-md ml-2 px-2 py-2 outline-none"
-                        placeholder='Search Country'
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="w-full pl-10 pr-4 py-2 border border-gray-400 rounded-md outline-none focus:ring-2 focus:ring-indigo-500"
+                        placeholder="Search Country"
                     />
                 </div>
             </div>
+
 
             <div className='w-full flex justify-between'>
                 <div className='w-3/4 grid grid-cols-4 gap-5 p-3'>
